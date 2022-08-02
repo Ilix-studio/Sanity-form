59032ccaeaf3cb35ce1078c37ce276abbf3fbc99
Ilix-studio <ilishjyoti17@gmail.com>
2022-08-02 11:10:15 +0530

    initial

---
+++ pages/api/users/register.js
@@ -0,0 +1,10 @@
+import nc from 'next-connect'
+import bcrypt from 'bcryptjs'
+import axios from 'axios'
+import config from '../../../utils/config'
+
+const handler = nc();
+
+handler.post()
+
+export default handler;
